81d5fab89803dafd4cdba0610cc1e74d6a6501a1
fmbo <contactfmb@gmail.com>
2014-11-22 23:20:52 -0500

    Add javascript to click button and ajax in form

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,26 +1,29 @@
-/* Author: 
-
+/* Author:
+	F.M. Bonnevier
 */
+$(document).ready(function() {
 
+	"use strict";
 
+	function ajaxForm () {
 
+		var $swapContainer = $('[data-swap-container]'),
+				$swapButton 		= $('[data-form-button]'),
 
+		bindButtonEvent = function () {
+			$swapButton.on('click', function (e) {
+				e.preventDefault();
+				// $.ajax({
+				// 	url: 'form.html'
+				// }).done(function (){
+				// 	console.log('did it');
+				// 	html.appendTo(swapContainer);
+				// });
+			});
 
+		};
+	}
 
+ajaxForm();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
