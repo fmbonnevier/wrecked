1292cf463193d81a0e6f7719a888a425c9229566
fmbo <contactfmb@gmail.com>
2014-11-23 22:48:23 -0500

    Add javascript to ajax form, add additional styles

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -13,15 +13,9 @@ $(document).ready(function() {
 		bindButtonEvent = function () {
 			$swapButton.on('click', function (e) {
 				e.preventDefault();
-				// $.ajax({
-				// 	url: 'form.html'
-				// }).done(function (){
-				// 	console.log('did it');
-				// 	html.appendTo(swapContainer);
-				// });
+				$( '.bubble').load( '/form.html .survey');
 			});
-
-		};
+		}();
 	}
 
 ajaxForm();
