1c3de1add60900c19d7d8d8ee42b6c5c2bc5657b
fmbo <contactfmb@gmail.com>
2014-11-23 23:07:22 -0500

    Remove readme

--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,26 +1,23 @@
-/* Author: 
-
+/* Author:
+	F.M. Bonnevier
 */
+$(document).ready(function() {
 
+	"use strict";
 
+	function ajaxForm () {
 
+		var $swapContainer = $('[data-swap-container]'),
+				$swapButton 		= $('[data-form-button]'),
 
+		bindButtonEvent = function () {
+			$swapButton.on('click', function (e) {
+				e.preventDefault();
+				$( '.bubble').load( '/form.html .survey');
+			});
+		}();
+	}
 
+ajaxForm();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
